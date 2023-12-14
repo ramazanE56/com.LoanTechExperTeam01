df3b91f52a587e2bb477eeb58a3a927dd68746d5
RamazanE56 <hantek5656@gmail.com>
2023-12-14 21:00:44 +0300

    14.12.2023ismail

--- src/test/java/pages/AdminLoginPage.java
+++ src/test/java/pages/AdminLoginPage.java
@@ -1,4 +1,10 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class AdminLoginPage {
+    private AdminLoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 }
