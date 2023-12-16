32f72216ffa0c32d5afce556210399dcaaa78d6e
halit <mehmethalit.metin@gmail.com>
2023-12-16 17:24:16 +0300

    halit 16/12/23 v1

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@wip",
+        tags = "@TC_04",
         dryRun = false
 )
 public class Runner {
