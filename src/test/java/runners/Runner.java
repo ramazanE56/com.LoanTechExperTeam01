f19fd6ae01c623aaa55ab14822f1f16648155272
MEHMETKAHRAMANTR <mehmetkahraman.2028@gmail.com>
2023-12-22 23:59:07 +0300

    son

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "stepdefinitions",
 
-        tags = "@smt",
+        tags = "@wip3",
         dryRun = false
 
 
